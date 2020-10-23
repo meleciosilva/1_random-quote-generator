bbeeff7a9c8c7662dfd736fe1ea566dbd2a4abb1
Mel <mpsilva94@gmail.com>
2020-10-23 16:50:50 -0400

    Created quotes array

---
+++ js/script.js
@@ -0,0 +1,70 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+
+let quotes = [
+  {
+    quote: 'Life is 10% what happens to you and 90% how you react to it.',
+    source: '',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'Comparison is the theif of joy.',
+    source: 'Theodore Roosevelt',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'Be yourself, not who others want you to be.',
+    source: '',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'You can\t blend in when you were born to stand out.',
+    source: '',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'Most of us ask advice when we know the answer, but want a different one.',
+    source: 'Ivern Ball',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: '',
+    source: '',
+    citation: '',
+    year: ''
+  },
+];
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
